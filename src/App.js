5cfbb85bbd50ed8a61afb4c6dc3af2c0f49121ab
MC <chokamoses2@gmail.com>
2024-03-07 18:25:39 +0300

    aded a working form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contacts from "./components/Contacts";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Skills />
       <Work />
+      <Contacts />
     </div>
   );
 }
