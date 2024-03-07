ba5d4aa6ced8854beec756c8cdb058819532e48a
MC <chokamoses2@gmail.com>
2024-03-07 19:03:34 +0300

    made the nav items responsive using react scroll

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -5,7 +5,7 @@ const Contacts = () => {
     <div name='contact' className='w-full h-screen bg-[#0a192f]  flex justify-center items-center p-4'>
         <form action="https://getform.io/f/nbdeegda" method='POST' className='flex flex-col max-w-[600px] w-full h-full'>
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+                <p className='text-4xl  inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form or shoot me an email - chokamoses2@gmail.com</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
